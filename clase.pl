e9666b6767272f56c8cea74e4bbcb326a53c2cfb
FrancoPandolfo <39636669+FrancoPandolfo@users.noreply.github.com>
2019-05-29 15:51:20 -0300

    este es el que tiene predicados

---
+++ clase.pl
@@ -0,0 +1,338 @@
+:- dynamic pelicula/5.
+
+pertenece(X,[X|_]).
+pertenece(X,[_|R]):- pertenece(X,R). 
+
+imprimir([]).
+imprimir( [A | B] ):- write(A),nl,
+imprimir(B). 	
+
+crear_lista(N,M,L):- 
+  findall(Num, between(N, M, Num), L).
+  
+actualizar_restultado(Resultado) :-
+	retractall(resultado(_)),
+	assert(resultado(Resultado)).
+
+pelicula('Titanic',1997,'drama','Leonardo Dicaprio').
+pelicula('The Lion King',1994,'animada','Matthew Broderick').
+pelicula('Forrest Gump',1994,'drama','Tom Hanks').
+pelicula('Toy Story',1995,'animada','Tom Hanks').
+pelicula('Edward Scissorhands',1990,'fantasia','Jhonny Depp').
+pelicula('Life Is Beautiful',1997,'drama','Roberto Benigni').
+pelicula('Gladiator',2000,'accion','Russel Crowe').
+pelicula('Monsters Inc.',2001,'animada','john Goodman').
+pelicula('Shrek',2001,'animada','Mike Myers').
+pelicula('Jurassic Park',1993,'cienciaFiccion','Sam Neill').
+pelicula('Ice Age',2002,'animada','john Leguizamo').
+pelicula('Pirates Of The Caribbean',2003,'fantasia','Johnny Depp').
+pelicula('Avatar',2009,'cienciaFiccion','Sam Worthington').
+pelicula('Harry Potter And The Philosopher Stone',2001,'fantasia','Daniel Radcliffe').
+pelicula('The Lord Of The Rings The Return Of The King',2003,'fantasia','Viggo Mortensen').
+pelicula('Braveheart',1995,'drama','Mel Gibson').
+pelicula('The Dark Knight',2008,'accion','Christian Bale').
+pelicula('Iam Legend',2007,'cienciaFiccion','Will Smith').
+pelicula('The Pianist',2002,'drama','Adrien Brody').
+pelicula('The Sixth Sense',1999,'drama','Bruce Willis').
+pelicula('Lord Of The Rings The Fellowship Of The Ring',2001,'fantasia','Viggo Mortensen').
+pelicula('The Shawshank Redemption',1994,'drama','Morgan Freeman').
+pelicula('Up',2009,'animada','Ed Esner').
+pelicula('Harry Potter And The Deadly Hallows Part 2',2011,'fantasia','Daniel Radcliffe').
+pelicula('Charlie And The Chocolate Factory',2005,'fantasia','Jhonny Depp').
+pelicula('Finding Nemo',2003,'animada','Albert Brooks').
+pelicula('Toy Story 3',2010,'animada','Tom Hanks').
+pelicula('Schindler''s List',1993,'drama','Liam Neeson').
+pelicula('The Avengers',2012,'cienciaFiccion','Robert Downey Jr').
+pelicula('Wall-E',2008,'animada','Ben Burtt').
+pelicula('Harry Potter And The Goblet Of Fire',2005,'fantasia','Daniel Radcliffe').
+pelicula('Beauty And The Beast',1991,'animada','Robby Benson').
+pelicula('The Silence Of The Lambs',1991,'thriller','Anthony Hopkins').
+pelicula('Harry Potter And The Prisioner Of Azkaban',2004,'fantasia','Daniel Radcliffe').
+pelicula('Intouchables',2011,'drama','Francois Cluzet').
+pelicula('Matrix',1999,'cienciaFiccion','Keanu Reeves').
+pelicula('The Lord Of The Rings The Two Towers',2002,'fantasia','Viggo Mortensen').
+pelicula('The Hunger Games',2012,'cienciaFiccion','Jennifer Lawrence').
+pelicula('Pulp Fiction',1994,'thriller','John Travolta').
+pelicula('Harry Potter And The Deadly Hallows Part 1',2010,'fantasia','Daniel Radcliffe').
+pelicula('Pirates Of The Caribbean At World End',2007,'fantasia','Johnny Depp').
+pelicula('Ratatouille',2007,'animada','Paton Oswalt').
+pelicula('Harry Potter And The Chamber Of Secrets',2002,'fantasia','Daniel Radcliffe').
+pelicula('Iron Man',2008,'cienciaFiccion','Robert Downey Jr').
+pelicula('Harry Potter And The Half Blood Prince',2009,'fantasia','Daniel Radcliffe').
+pelicula('Despicable Me',2010,'animada','Steve Carell').
+pelicula('Fight Club',1999,'drama','Brad Pitt').
+pelicula('The Black Swan',2010,'thriller','Natalie Portman').
+pelicula('Harry Potter And The Order Of the Phoneix',2007,'fantasia','Daniel Radcliffe').
+pelicula('Dark Knight Rises',2012,'accion','Christian Bale').
+pelicula('Inception',2010,'supspenso','Leonardo Dicaprio').
+pelicula('American History X',1998,'drama','Edward Norton').
+pelicula('Pirates Of The Caribbean Dead Man''s Chest',2006,'fantasia','Johnny Depp').
+pelicula('The Incredibles',2004,'animada','Samuel L Jackson').
+pelicula('Shrek 2',2004,'animada','Mike Myers').
+pelicula('Tangled',2010,'animada','Mandy Moore').
+pelicula('Sleepy Hollow',1999,'terror','Johnny Depp').
+pelicula('V For Vendetta',2005,'cienciaFiccion','Natalie Portman').
+pelicula('The Simpsons Movie',2007, 'animada','Humberto Velez').
+pelicula('Corpse Bride',2005,'animada','Johnny Depp').
+pelicula('Pirates Of The Caribbean Saling in Mystirious Waters',2011,'fantasia','Johnny Depp').
+pelicula('Ice Age 2',2006,'animada','John Leguizamo').
+pelicula('A Beautiful Mind',2001,'biografia','Russell Crowe').
+pelicula('El Viaje De Chihiro',2001,'animada','Lucila Gomez').
+pelicula('Alice In Wonderland',2010,'fantasia','Johnny Depp').
+pelicula('Spider-Man',2002,'cienciaFiccion','Tobey Maguire').
+pelicula('Madagascar',2005,'animada','Lucila Gomez').
+pelicula('Life Of Pi',2012,'aventura','Suraj Sharma').
+pelicula('Million Dollar Baby',2004,'drama','Clint Eastwood').
+pelicula('The Others',2001,'terror','Nicole Kidman').
+pelicula('X-Men',2000,'cienciaFiccion','Hugh Jackman').
+pelicula('The Nightmare Before Christmas',1993,'animada','Danny Elfman').
+pelicula('Brave',2012,'animada','Kelly Macdonald').
+pelicula('Cars',2006,'animada','Owen Wilson').
+pelicula('Kill Bill Volume 1',2003,'accion','Uma Thurman').
+pelicula('Spider-Man 2',2004,'cienciaFiccion','Tobey Maguire').
+pelicula('Star Wars The Revenge Of The Sith',2005,'cienciaFiccion','Ewan McGregor').
+pelicula('Spider-Man 3',2007,'cienciaFiccion','Tobey Maguire').
+pelicula('X-Men First Class',2011,'cienciaFiccion','James McAvoy').
+pelicula('The Help',2011,'drama','Emma Stone').
+pelicula('Star Wars The Phantom Menace',1999,'cienciaFiccion','Ewan McGregor').
+pelicula('Kill Bill Volume 2',2004,'accion','Uma Thurman').
+pelicula('Coraline',2009,'animada','Dakota Fanning').
+pelicula('Star Wars Attack Of The Clones',2002,'cienciaFiccion','Ewan McGregor').
+pelicula('Big Fish',2003,'aventura','Ewan McGregor').
+pelicula('War Horse',2011,'belica','Tom Hiddlestone').
+pelicula('Les Miserables',2012,'musical','Hugh Jackman').
+pelicula('GoodFellas',1990,'drama','Robert De Niro').
+pelicula('The Godfather Part 3',1990,'drama','Al Pacino').
+pelicula('Reservoir Dogs',1992,'crimen','Quentin Tarantino').
+pelicula('Aladdin',1992,'animada','Robin Williams').
+pelicula('Batman Returns',1992,'cienciaFiccion','Michael Keaton'). 
+pelicula('From Dusk Till Dwan',1996,'terror','Quentin Tarantino'). 
+pelicula('Fargo',1996,'thriller','Steve Bucemi'). 
+pelicula('Jerry Maguire',1996,'drama','Tom Cruise').
+pelicula('Mission Impossible',1996,'accion','Tom Cruise').
+pelicula('The Wolf Of Wall Street',2013,'biografia','Leonardo Dicaprio'). 
+pelicula('Her',2013,'romance','Joaquin Phoenix'). 
+pelicula('Boyhood',2014,'drama','Elson Coltrane').
+pelicula('Birdman',2014,'drama','Michael Keaton').
+pelicula('Whiplash',2014,'drama','J K Simmons').
+pelicula('Ex Machina',2014,'cienciaFiccion','Oscar Isaac').
+pelicula('The Revenant',2015,'drama','Leonardo Dicaprio').
+pelicula('Mad Max',2015,'cienciaFiccion','Tom Hardy').
+pelicula('Sicario',2015,'policial','Benicio del Toro').
+pelicula('Spotlight',2015,'biografia','Michael Keaton').
+pelicula('The Hateful Eight',2015,'suspenso','Samuel L Jackson').
+pelicula('Kubo And The Two Strings',2016,'animada','Art Parkinson').
+pelicula('Hacksaw Ridge',2016,'biografia','Andrew Garfield'). 
+pelicula('Rogue One',2016,'cienciaFiccion','Felicity Jones').
+pelicula('Blade Runner 2049',2017,'cienciaFiccion','Ryan Gosling').
+pelicula('The Shape Of Water',2017,'fantasia','Sally Hawkins').
+pelicula('Three Billaboards Outside Ebbing Missouri',2017,'drama','Frances McDormand').
+pelicula('Dunkirk',2017,'belica','Tom Hardy').
+pelicula('Mandy',2018,'thriller','Nicolas Cage').
+pelicula('Annihilation',2018,'cienciaFiccion','Natalie Portman').
+pelicula('The Rider',2017,'drama','Lane Scott').
+pelicula('Saving Private Ryan',1998,'belica','Tom Hanks').
+pelicula('Seven',1995,'thriller','Brad Pitt').
+pelicula('Green Mile',1999,'drama','Tom Hanks').
+pelicula('Leon The Professional',1994,'drama','Natalie Portman').
+pelicula('The Fith Element',1997,'cienciaFiccion','Bruce Willis').
+pelicula('La Momia',1999,'aventura','Brendan Fraser').
+pelicula('Memento',2000,'thriller','Carrie Anne Moss').
+pelicula('Requiem For A Dream',2000,'thriller','Jennifer Connely').
+pelicula('Tropa De Elite',2007,'accion','Wagner Moura').
+pelicula('Tropa De Elite 2',2010,'accion','Wagner Moura').
+pelicula('Inglorious Bastards',2009,'belica','Quentin Tarantino'). 
+pelicula('The Prestige',2006,'suspenso','Hugh Jackman').
+pelicula('El Secreto De Sus Ojos',2009,'drama','Ricardo Darin').
+pelicula('No Country For Old Men',2007,'drama','Josh Brolin').
+pelicula('Sin City',2005,'drama','Jessica Alba').
+pelicula('Slamdog Millionaire',2008,'drama','Dev Patel').
+pelicula('Snatch',2000,'comedia','Brad Pitt').
+pelicula('Ip Man',2008,'accion','Donnie Yen').
+pelicula('In Bruges',2008,'drama','Colin Farrell').
+pelicula('Blood Diamond',2006,'drama','Leonardo Dicaprio').
+pelicula('Casino Royale',2006,'accion','Daniel Craig').
+pelicula('Shutter Island',2010,'thriller','Leonardo Dicaprio').
+
+generos(Generos) :- Generos = ([
+	'accion',
+	'animada',
+	'aventura',
+	'belica',
+	'biografia',
+	'cienciaFiccion',
+	'comedia',
+	'crimen',
+	'drama',
+	'fantasia',
+	'musical',
+	'policial',
+	'romance',
+	'suspenso',
+	'terror',
+	'thriller',
+	'todos'
+]).
+
+actores(Actores) :- Actores = (['Adrien Brody',
+'Al Pacino',
+'Albert Brooks',
+'Andrew Garfield',
+'Anthony Hopkins',
+'Art Parkinson',
+'Ben Burtt',
+'Ben Stiller',
+'Benicio Del Toro',
+'Brad Pitt',
+'Brendan Fraser',
+'Bruce Willis',
+'Carrie Anne Moss',
+'Christian Bale',
+'Clint Eastwood',
+'Colin Farrell',
+'Dakota Fanning',
+'Daniel Craig',
+'Daniel Radcliffe',
+'Danny Elfman',
+'Dev Patel',
+'Donnie Yen',
+'Ed Esner',
+'Edward Norton',
+'Elson Coltrane',
+'Emma Stone',
+'Ewan McGregor',
+'Felicity Jones',
+'Frances McDormand',
+'Francois Cluzet',
+'Hugh Jackman',
+'Humberto Velez',
+'J K Simmons',
+'James McAvoy',
+'Jennifer Connely',
+'Jennifer Lawrence',
+'Joaquin Phoenix',
+'John Goodman',
+'John Leguizamo',
+'John Travolta',
+'Johnny Depp',
+'Josh Brolin',
+'Keanu Reeves',
+'Kelly Macdonald',
+'Lane Scott',
+'Leonardo Dicaprio',
+'Liam Neeson',
+'Lucila Gomez',
+'Mandy Moore',
+'Matthew Broderick',
+'Mel Gibson',
+'Michael Keaton',
+'Mike Myers',
+'Morgan Freeman',
+'Natalie Portman',
+'Nicolas Cage',
+'Nicole Kidman',
+'Oscar Isaac',
+'Owen Wilson',
+'Paton Oswalt',
+'Quentin Tarantino',
+'Ricardo Darin',
+'Robby Benson',
+'Robert De Niro',
+'Robert Downey Jr',
+'Roberto Benigni',
+'Robin Williams',
+'Russel Crowe',
+'Ryan Gosling',
+'Sally Hawkins',
+'Sam Neil',
+'Sam Worthington',
+'Samuel L Jackson',
+'Steve Bucemi',
+'Steve Carell',
+'Suraj Sharma',
+'Tobey Maguire',
+'Tom Cruise',
+'Tom Hanks',
+'Tom Hardy',
+'Tom Hiddlestone',
+'Uma Thurman',
+'Viggo Mortensen',
+'Wagner Moura',
+'Will Smith','todos']).
+
+menu :- write(''),nl,write('***Recomendador de Peliculas***'), nl,
+		write('ingrese el numero de opcion'),nl,
+		write('1. Iniciar'),nl,
+		write('2. salir'),nl,
+		read(OpcionMenu),
+		inicio(OpcionMenu).
+		
+inicio(OpcionMenu) :- OpcionMenu == 1, iniciarPrograma, menu;
+					  OpcionMenu > 2 , menu;
+					  OpcionMenu < 1, menu;
+					  OpcionMenu == 2, true.
+
+iniciarPrograma :-
+
+		write(''),nl,write('ingrese un rango de anios para elegir una pelicula'),nl,nl,
+		write('ingrese anio de inicio'),nl,nl,
+		read(AnioInicial),
+		write('ingrese anio de fin'),nl,
+		read(AnioFinal),
+		validarAnio(AnioInicial,AnioFinal).
+		
+validarAnio(AnioInicial,AnioFinal) :-
+		((AnioInicial < 1990 ; AnioFinal > 2018; AnioInicial > AnioFinal ) -> errorIngresoAnio);
+		((AnioInicial > 1989 , AnioFinal < 2019 , AnioInicial < AnioFinal) -> elegirAnio(AnioInicial,AnioFinal)).
+
+errorIngresoAnio :- 
+		write('el rango de anios ingresado es incorrecto'),nl,
+		iniciarPrograma.
+		
+elegirAnio(AnioInicial,AnioFinal) :-
+		write(''),nl,write('rango de anios:'),
+		write(AnioInicial),
+		write('-'),
+		write(AnioFinal),nl,nl, ingresoGenero(AnioInicial,AnioFinal).
+		
+ingresoGenero(AnioInicial,AnioFinal) :-
+		write('ingrese el genero que quiere sea su pelicula'),nl,
+		generos(Generos),imprimir(Generos),
+		read(Genero),
+		validarGenero(Genero,AnioInicial,AnioFinal).
+
+validarGenero(Genero,AnioInicial,AnioFinal) :-
+		((generos(Generos),pertenece(Genero,Generos)) -> elegirGenero(Genero,AnioInicial,AnioFinal));
+		((generos(Generos),\+pertenece(Genero,Generos)) -> errorGenero).
+	
+errorGenero :-
+		write('el genero ingresado no esta en la base de datos'),nl,ingresoGenero.
+
+elegirGenero(Genero,AnioInicial,AnioFinal) :-
+		write(''),nl,write('el genero que eligio es '),
+		write(Genero),nl, ingresoActor(Genero,AnioInicial,AnioFinal).
+		
+ingresoActor(Genero,AnioInicial,AnioFinal) :-
+		write(''),nl,write('ingrese al actor que quiere en su pelicula entre comillas ej: ''Nicolas Cage'''),nl,
+		actores(Actores),imprimir(Actores),
+		read(Actor),
+		validarActor(Actor,Genero,AnioInicial,AnioFinal).
+		
+validarActor(Actor,Genero,AnioInicial,AnioFinal) :-
+		((actores(Actores), pertenece(Actor,Actores)) -> elegirActor(Actor,Genero,AnioInicial,AnioFinal));
+		((actores(Actores), \+pertenece(Actor,Actores)) -> errorActor).
+		
+errorActor :-
+		write('el actor ingresado no esta en la base de datos'),nl,ingresoActor.
+		
+elegirActor(Actor,Genero,AnioInicial,AnioFinal) :-
+		write(''),nl,write('el actor que eligio es '),
+		write(Actor),nl,devolverResultado(AnioInicial,AnioFinal,Genero,Actor).
+		
+devolverResultado(AnioInicial,AnioFinal,Genero,Actor) :-
+			  crear_lista(AnioInicial, AnioFinal, RangoAnios),
+			  ListaEncontradas = [],
+			  forall(member(Anio,RangoAnios), findall(Anio, pelicula(Peli, Anio, Gen, Act), ListaEncontradas)),
+			  imprimir(ListaEncontradas).
+	
+		
